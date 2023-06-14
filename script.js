1e1fb1cbae59662c8be975c622626c50ead2717c
Sérgio Rogério <rogerio.bunker@gmail.com>
2023-06-13 21:44:58 -0300

    falta as image do carrosel

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Aqui você pode adicionar scripts personalizados, se necessário
+function startCarousel() {
+  const banner = document.querySelector('.banner');
+  const images = banner.querySelectorAll('img');
+  let currentIndex = 0;
+
+  function showImage(index) {
+    images.forEach((image, i) => {
+      if (i === index) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
+  }
+
+  function nextImage() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+    showImage(currentIndex);
+  }
+
+  setInterval(nextImage, 6000);
+}
+
+startCarousel();
